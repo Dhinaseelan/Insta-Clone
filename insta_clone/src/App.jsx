e4f4155f362975b9432f8222b1fb462061feaca3
Dhinaseelan <dhinaseeland@gmail.com>
2026-02-24 23:26:26 +0530

    creating partion and setup

---
+++ insta_clone/src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+
+  return (
+    <>
+      <div className="d-flex bg-primary vh-100">
+        <div className="w-20">menu</div>
+        <div className="w-50 bg-white">feed</div>
+        <div className="w-30">suggestions</div>
+      </div>
+    </>
+  )
+}
+
+export default App
