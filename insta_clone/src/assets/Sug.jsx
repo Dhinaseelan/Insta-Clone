5305b5e46b616c56bd5653ef0a36aad3b76b5642
Dhinaseelan <dhinaseeland@gmail.com>
2026-02-27 17:08:37 +0530

    feat:suggestion component complite peple profile follow

--- insta_clone/src/assets/Sug.jsx
+++ insta_clone/src/assets/Sug.jsx
@@ -14,23 +14,49 @@ const Sug = () => {
       .then((res) => res.json())
       .then((data) => {
         console.log("Profile data:", data);
-        setProfile(data);
+        setProfile(data[0]);
       })
       .catch((err) => console.log(err));
   }, []);
 
   return (
     <div>
-      {profile && (
+      <div className="suggestion m-4 position-fixed">
+        {profile && (
+          <div className="d-flex mt-2">
+            <img
+              className="rounded-circle me-2"
+              src={profile.profilePic}
+              alt={profile.username || "profile"}
+            />
+            <h5>{profile.username}</h5>
+            <small className="ms-auto text-primary">switch</small>
+          </div>
+        )}
         <div className="d-flex mt-2">
-          <img
-            className="rounded-circle me-2"
-            src={profile.profilePic}
-            alt={profile.username || "profile"}
-          />
-          <h5>{profile.username}</h5>
+          <small>Suggestions for you</small>
+          <b className="ms-auto text-primary">See all</b>
         </div>
-      )}
+        {suggestions.length > 0 ? (
+          <div>
+            {suggestions.map((suggestion) => (
+              <div key={suggestion.id}>
+                <div className="d-flex mt-4 mb-auto">
+                  <img
+                    className="rounded-circle me-2 "
+                    src={suggestion.profilePic}
+                    alt=""
+                  />
+                  <h6 className="font-size-small">{suggestion.username}</h6>
+                  <small className="ms-auto text-primary">follow</small>
+                </div>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div>no suggestions</div>
+        )}
+      </div>
     </div>
   );
 };
