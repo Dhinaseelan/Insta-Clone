4a88581ebb9ba80d9d37afa01b0bdec43b262cb9
Dhinaseelan <dhinaseeland@gmail.com>
2026-03-06 19:12:51 +0530

    Fix router basename for GitHub Pages

--- insta_clone/src/main.jsx
+++ insta_clone/src/main.jsx
@@ -4,21 +4,27 @@ import App from "./App.jsx";
 import ViewStory from "./ViewStory.jsx";
 import "./index.css";
 import Profile from "./Profile.jsx";
-const router = createBrowserRouter([
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />
+    },
+    {
+      path: "/story/:id/:tot",
+      element: <ViewStory />,
+      errorElement: <p>Story not found</p>
+    },
+    {
+      path: "/profile",
+      element: <Profile />
+    }
+  ],
   {
-    path: "/",
-    element: <App />
-  },
-  {
-    path: "/story/:id/:tot",
-    element: <ViewStory />,
-    errorElement: <p>Story not found</p>
-  },
-  {
-    path:"/profile",
-    element:<Profile/>
+    basename: "/Insta-Clone"   // 👈 important for GitHub Pages
   }
-]);
+);
 
 createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
